352cd55190bf97dc31b9ca770d47ff966947942c
Tayfun Koç <tyfnkc@gmail.com>
2019-04-03 15:58:41 +0300

    create project

---
+++ ExampleForAddOn-VisualStudio/XMLFormatterHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ExampleForAddOn_VisualStudio
+{
+    class XMLFormatterHelper
+    {
+        public static string Format(string text)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                xmlDoc.LoadXml(text);
+                using (XmlTextWriter writer = new XmlTextWriter(ms, Encoding.Unicode))
+                {
+                    // format content
+                    writer.Formatting = Formatting.Indented;
+                    xmlDoc.WriteContentTo(writer);
+                    writer.Flush();
+                    ms.Position = 0; 
+
+                    StreamReader sr = new StreamReader(ms);
+                    string formatted = sr.ReadToEnd();
+                    Debug.Print(formatted);
+                    return formatted;
+                }
+            }
+        }
+    }
+}
---
+++ ExampleForAddOn-VisualStudio/XmlFomatter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace ExampleForAddOn_VisualStudio
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class XmlFomatter
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("01c7e044-cff3-461f-adee-daf21d03b7dd");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlFomatter"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private XmlFomatter(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static XmlFomatter Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in XmlFomatter's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new XmlFomatter(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "XmlFomatter";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+
+            DTE dte = Package.GetGlobalService(typeof(DTE)) as DTE;
+            Document doc = dte.ActiveDocument;
+            TextDocument textDocument = doc.Object() as TextDocument;
+            Debug.Print(textDocument.Language);
+            if (textDocument.Language.Equals("XML"))
+            {
+                var p = textDocument.StartPoint.CreateEditPoint();
+                string text = p.GetText(textDocument.EndPoint);
+                string formatted = XMLFormatterHelper.Format(text);
+                p.Delete(textDocument.EndPoint);
+                p.Insert(formatted);
+                dte.ActiveDocument.Save();
+            }
+
+        }
+    }
+}
